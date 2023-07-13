5c09388c9bf068e2e2babfe52972640b4f19df45
wadpas <wadpas@gmail.com>
2023-07-13 11:58:36 +0300

    added header component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Store
+# Delta Store
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.12.
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  template: `
-    <p>
-      header works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styles: [],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
