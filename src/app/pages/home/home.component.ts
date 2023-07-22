bf5f724c6b5cee083bbea61458f2b6e271a4bd8a
wadpas <wadpas@gmail.com>
2023-07-22 19:17:09 +0300

    added products-header.component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,15 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styles: [],
 })
 export class HomeComponent implements OnInit {
+  cols = 3;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onColumnsCountChange(colsNumber: number): void {
+    this.cols = colsNumber;
+  }
 }
