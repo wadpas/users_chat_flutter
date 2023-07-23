4434881d8d75d09953318da98541bb51452e4296
wadpas <wadpas@gmail.com>
2023-07-23 10:41:18 +0300

    added product-box.component

---
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-box",
+  templateUrl: "product-box.component.html",
+})
+export class ProductBoxComponent implements OnInit {
+  @Input() fullWidthMode = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
+const ROWS_HEIGT: { [id: number]: number } = { 1: 400, 3: 350, 4: 300 };
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGT[this.cols];
   category: string | undefined;
 
   constructor() {}
@@ -14,6 +17,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNumber: number): void {
     this.cols = colsNumber;
+    this.rowHeight = ROWS_HEIGT[this.cols];
   }
   onShowCategory(category: string): void {
     this.category = category;
