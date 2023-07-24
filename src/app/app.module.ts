382030f0cb383f1b5c5b905474192da66020e95b
wadpas <wadpas@gmail.com>
2023-07-24 10:20:10 +0300

    added cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,14 +17,23 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
-import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
-import { FiltersComponent } from './pages/home/components/filters/filters.component';
-import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { ProductsHeaderComponent } from "./pages/home/products-header/products-header.component";
+import { FiltersComponent } from "./pages/home/filters/filters.component";
+import { ProductBoxComponent } from "./pages/home/product-box/product-box.component";
+import { CartComponent } from "./pages/cart/cart.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsHeaderComponent,
+    FiltersComponent,
+    ProductBoxComponent,
+    CartComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "src/app/models/cart_models";
+
+@Component({
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Keyboard",
+        price: 150,
+        quantity: 1,
+        id: 1,
+      },
+      {
+        product: "https://via.placeholder.com/150",
+        name: "Mouse",
+        price: 135,
+        quantity: 2,
+        id: 2,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map(item => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
+}
